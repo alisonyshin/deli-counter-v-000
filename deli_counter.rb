a8a2eb2c09b6c3f5492a2110225fc759e51c9ac2
Alison Shin <alisonyshin@gmail.com>
2018-07-30 07:40:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+
+def line(queu)
+  if queu.empty?
+    puts "The line is currently empty."
+  else
+    queu.each_with_index{|name,index| puts "#{index+1}. #{name}"}
+  end
+end
+
+
+def take_a_number(queu,name)
+  queu.push(name)
+  puts "Welcome, #{name}. You are number #{queu.index(name)+1} in line."
+end
+
+def now_serving(queu)
+  if queu.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{queu.shift()}."
+  end
+end
+
+#now_serving(katz_deli)
