9799faa6302cd5b86707e94395e73f3d633f23f2
Alison Shin <alisonyshin@gmail.com>
2018-07-30 07:49:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(queu)
   if queu.empty?
     puts "The line is currently empty."
   else
+    queu_ordered=[]
     queu.each_with_index.map do |name, index|
       queu_ordered = (index+1).to_s + ". " + name.to_s + " "
     end
