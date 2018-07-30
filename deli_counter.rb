a80c45ba6f8d215fad8b8c95ffff6ee4ccdeab93
Alison Shin <alisonyshin@gmail.com>
2018-07-30 07:49:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(queu)
   else
     queu_ordered=[]
     queu.each_with_index.map do |name, index|
-      queu_ordered = (index+1).to_s + ". " + name.to_s + " "
+      queu_ordered << (index+1).to_s + ". " + name.to_s + " "
     end
     puts queu_ordered
   end
